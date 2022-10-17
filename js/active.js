beef8a8d57270b8bdc1345cd5c9685200c40548a
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-10-18 00:07:51 +0600

    commit

---
+++ js/active.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+
+    $('.slider-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fa-sharp fa-solid fa-chevron-left"></i>','<i class="fa-sharp fa-solid fa-chevron-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+})
