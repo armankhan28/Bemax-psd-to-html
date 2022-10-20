5b09cf76b612a85bd1943935ba8c3c9833ab04c8
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-10-20 18:50:46 +0600

    commit

--- js/active.js
+++ js/active.js
@@ -18,4 +18,20 @@ $(document).ready(function() {
             }
         }
     })
+    $('.project-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fa-sharp fa-solid fa-chevron-left"></i>','<i class="fa-sharp fa-solid fa-chevron-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
 })
