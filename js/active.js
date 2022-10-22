3912a3d40c40ced134c847528559862cc76f5d63
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-10-23 02:06:09 +0600

    commit

--- js/active.js
+++ js/active.js
@@ -58,6 +58,24 @@ $(document).ready(function() {
             }
         }
     })
+    // testi-area
+
+    $('.testi-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fa-sharp fa-solid fa-chevron-left"></i>','<i class="fa-sharp fa-solid fa-chevron-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 
 
     // brand area
