b68303cf6429699b627ff3f6e8663ea73244c944
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-10-22 10:01:14 +0600

    commit

--- js/active.js
+++ js/active.js
@@ -40,6 +40,25 @@ $(document).ready(function() {
         }
     })
 
+    // blog-area
+
+    $('.blog-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fa-sharp fa-solid fa-chevron-left"></i>','<i class="fa-sharp fa-solid fa-chevron-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
 
 
     // video popup
